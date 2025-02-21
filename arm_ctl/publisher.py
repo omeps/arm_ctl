e3cd61c72e1486c05333591fd0060d19db2a9b49
omeps <ethan.ben.yehuda0@gmail.com>
2025-02-21 14:13:49 -0800

    init

---
+++ arm_ctl/publisher.py
@@ -0,0 +1,60 @@
+import rclpy
+import argparse
+import numpy as np
+from rclpy.node import Node
+import dynamixel_sdk
+import std_msgs.msg as msg
+id_base = 5
+id_linkage_a = 6 
+id_linkage_b = 7
+
+class ArmModelPublisher(Node):
+    # MAKE SURE `dir' ends with a / to keep topics in 1 directory
+    def __init__(self, dir="arm_ctl/", timer_period=0.03, device_name='/dev/ttyUSB0'):
+        super().__init__('arm_model_publisher',parameter_overrides=[])
+        self.publish_orientation_base = self.create_publisher(
+            msg.UInt32, dir + "base", 1)
+        self.publish_orientation_linkage_a = self.create_publisher(
+            msg.UInt32, dir + "linkage_a", 1)
+        self.publish_orientation_linkage_b = self.create_publisher(
+            msg.UInt32, dir + "linkage_b", 1)
+        self.timer = self.create_timer(timer_period, self.publish_motor_data)
+
+        self.port_handler = dynamixel_sdk.PortHandler(device_name)
+        self.packet_handler = dynamixel_sdk.PacketHandler(2.0)
+        self.port_handler.openPort() or quit()
+        self.port_handler.setBaudRate(1000000)
+        toggle_torque = 64
+        for id in [id_base, id_linkage_a, id_linkage_b]:
+            self.packet_handler.write1ByteTxRx(self.port_handler, id, toggle_torque, 0) # turn motors off so they can be moved
+
+    def publish_motor_data(self):
+        global id_base,id_linkage_a, id_linkage_b
+        present_position = 132
+        orientation_base = self.packet_handler.read4ByteTxRx(self.port_handler, id_base, present_position)[0]
+        orientation_linkage_a = self.packet_handler.read4ByteTxRx(self.port_handler, id_linkage_a, present_position)[0]
+        orientation_linkage_b = self.packet_handler.read4ByteTxRx(self.port_handler, id_linkage_b, present_position)[0]
+        payload = msg.UInt32()
+        payload.data = orientation_base
+        self.publish_orientation_base.publish(payload)
+        payload.data = orientation_linkage_a
+        self.publish_orientation_linkage_a.publish(payload)
+        payload.data = orientation_linkage_b
+        self.publish_orientation_linkage_b.publish(payload)
+
+def main(args=None):
+    rclpy.init(args=args)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dir", help="ros topic directory", default="arm_ctl")
+    parser.add_argument("--period", help="how long to publish values (in seconds)", default="0.03")
+    parser.add_argument("--device_name", help="device file location", default="/dev/ttyUSB0")
+    args = parser.parse_args()
+    if args.dir[-1] != '/': args.dir += '/'
+    publisher = ArmModelPublisher(dir=args.dir, timer_period=float(args.period), device_name=args.device_name)
+    rclpy.spin(publisher)
+    publisher.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
---
+++ arm_ctl/subscriber.py
@@ -0,0 +1,87 @@
+import rclpy
+import numpy as np
+def overflow_add(a,b):
+
+    return int(np.array(a,dtype=np.int32) + np.array(b,dtype=np.int32))
+import argparse
+from rclpy.node import Node
+import dynamixel_sdk
+import std_msgs.msg as msg
+id_base = 5
+id_linkage_a = 6 
+id_linkage_b = 7
+
+class ArmSubscriber(Node):
+    # MAKE SURE `dir' ends with a / to keep topics in 1 directory
+    def __init__(self, dir="arm_ctl/", device_name='/dev/ttyUSB0'):
+        super().__init__('arm_subscriber',parameter_overrides=[])
+        self.port_handler = dynamixel_sdk.PortHandler(device_name)
+        self.packet_handler = dynamixel_sdk.PacketHandler(2.0)
+        self.port_handler.openPort() or quit()
+        self.port_handler.setBaudRate(1000000)
+        toggle_torque = 64
+        for id in [id_base, id_linkage_a, id_linkage_b]:
+            self.packet_handler.write1ByteTxRx(self.port_handler, id, toggle_torque, 0) # turn motors off to configure EEPROM
+            self.packet_handler.write4ByteTxRx(self.port_handler, id, 112, 100) # turn motors off to configure EEPROM
+            self.packet_handler.write1ByteTxRx(self.port_handler, id, toggle_torque, 1) # turn motors on
+        self.subscription_base = self.create_subscription(
+            msg.UInt32,
+            dir + 'base',
+            self.move_base,
+            1
+        )
+        self.subscription_linkage_a = self.create_subscription(
+            msg.UInt32,
+            dir + 'linkage_a',
+            self.move_linkage_a,
+            1
+        )
+        self.subscription_linkage_b = self.create_subscription(
+            msg.UInt32,
+            dir + 'linkage_b',
+            self.move_linkage_b,
+            1
+        )
+
+    def move_base(self, payload):
+        goal_position = 116
+        self.packet_handler.write4ByteTxRx(
+            self.port_handler,
+            id_base,
+            goal_position,
+            payload.data
+        )
+    def move_linkage_a(self, payload):
+        goal_position = 116
+        self.packet_handler.write4ByteTxRx(
+            self.port_handler,
+            id_linkage_a,
+            goal_position,
+            payload.data
+        )
+    def move_linkage_b(self, payload):
+        goal_position = 116
+        self.packet_handler.write4ByteTxRx(
+            self.port_handler,
+            id_linkage_b,
+            goal_position,
+            payload.data
+        )
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dir", help="ros topic directory", default="arm_ctl")
+    parser.add_argument("--period", help="how long to publish values (in seconds)", default="0.03")
+    parser.add_argument("--device_name", help="device file location", default="/dev/ttyUSB0")
+    args = parser.parse_args()
+    if args.dir[-1] != '/': args.dir += '/'
+    subscriber = ArmSubscriber(dir=args.dir, device_name=args.device_name)
+    rclpy.spin(subscriber)
+    subscriber.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
